0a4d91bdf7b9ba438de5da417f11ee3fff29e613
Sinfjell <52018161+Sinfjell@users.noreply.github.com>
2023-08-21 15:26:59 +0200

    performed task #1 to #6

--- assignment1.R
+++ assignment1.R
@@ -1,10 +1,35 @@
+# assignment urls
+  # https://hotneim.github.io/ban400/assignment-01.html
+
 # Load necessary libraries
 library(readxl)
 library(dplyr)
 
-# Assuming the file is located in your current working directory
+# 1: Read dataset
 data_nasdaq <- read_excel("data-nasdaq-returns.xls")
 head(data_nasdaq)
 
+# 2: Custom data frame
 selected_data <- data_nasdaq %>% select(Date, NASDAQ, AAPL)
 head(selected_data)
+
+# 3: Scatterplot
+plot(selected_data$NASDAQ, selected_data$AAPL, 
+     xlab="NASDAQ Returns", 
+     ylab="Apple Returns", 
+     main="Scatterplot of NASDAQ vs. Apple Returns")
+
+#4: Create sign columns
+# function mutate is used to add new columns (variables) to the data frame
+selected_data <- selected_data %>%
+  mutate(sign_NASDAQ = sign(NASDAQ),
+         sign_AAPL = sign(AAPL))
+head(selected_data)
+
+#5: 
+selected_data <- selected_data %>% 
+  mutate(sum = (sign_NASDAQ+sign_AAPL)/2)
+head(selected_data)
+
+#6: 
+
